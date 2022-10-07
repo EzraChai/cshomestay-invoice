887c1812951358d451a639c543465f6a00f48d54
Ezra Chai <juanzhe2@gmail.com>
2022-10-07 20:06:15 +0800

    added many things

--- components/DisplayInvoice.tsx
+++ components/DisplayInvoice.tsx
@@ -4,8 +4,8 @@ export const DisplayInvoice = ({ data }: any) => {
     <div className="grid grid-cols-4 max-w-5xl mx-auto mt-20">
       <div className="col-span-2">
         <div className="text-4xl font-extrabold uppercase">Invoice</div>
-        <div className="font-bold text-md mt-4">Cozy & Sweet Homestay</div>
-        <div className="font-normal mt-1">
+        <div className="font-bold text-md mt-8">Cozy & Sweet Homestay</div>
+        <div className="font-normal mt-4">
           Bayview Villas Port Dickson <br /> Bt 7, Jalan Pantai Teluk Kemang{" "}
           <br /> 71050 Sirusa Port Dickson <br /> Negeri Sembilan Darul Khusus
         </div>
@@ -13,9 +13,12 @@ export const DisplayInvoice = ({ data }: any) => {
       <div></div>
       <div className="flex flex-col items-center">
         <div className="">logo</div>
-        <div className="text-center ">+6019 3099 359</div>
+        <div className="text-center">
+          {" "}
+          <span className="font-semibold">Telephone </span> +6019 3099 359
+        </div>
       </div>
-      <div className=" col-span-4 mt-8">
+      <div className=" col-span-4 mt-16">
         <table className="w-full">
           <thead className="border-t-2 border-b-2 border-stone-800 font-semibold uppercase">
             <tr className="text-center p-4">
@@ -27,13 +30,15 @@ export const DisplayInvoice = ({ data }: any) => {
           <tbody className="text-center">
             <tr>
               <td className="flex">
-                <table className="w-full mt-2">
+                <table className="w-full mt-4">
                   <tbody>
                     <tr>
                       <td className="w-1/3 text-left font-semibold">
                         Customer Name
                       </td>
-                      <td className="w-2/3 text-left">{data.customerName}</td>
+                      <td className="w-2/3 text-left">
+                        {data.prefix} {data.customer}
+                      </td>
                     </tr>
                   </tbody>
                 </table>
@@ -41,12 +46,12 @@ export const DisplayInvoice = ({ data }: any) => {
               <td></td>
             </tr>
             <tr>
-              <table className="w-full mt-2">
+              <table className="w-full mt-4">
                 <tbody>
                   <tr>
                     <td className="w-1/3 text-left font-semibold">Period</td>
                     <td className="w-2/3 text-left">
-                      {/* {data.day[0]} - {data.day[1]} {data.diffTime} night(s) */}
+                      {data.day[0]} - {data.day[1]} {data.diffTime} night(s)
                     </td>
                   </tr>
                 </tbody>
@@ -55,30 +60,61 @@ export const DisplayInvoice = ({ data }: any) => {
               <td></td>
             </tr>
             <tr>
-              <table className="w-full mt-2">
-                <tr>
-                  <td className="w-1/3 text-left font-semibold">
-                    Total Charges
-                  </td>
-                </tr>
+              <table className="w-full mt-4">
+                <tbody>
+                  <tr>
+                    <td className="w-1/3 text-left font-semibold">
+                      Total Charges
+                    </td>
+                  </tr>
+                </tbody>
               </table>
               <td></td>
-              <td>{parseInt(data.pricePerDay).toFixed(2)}</td>
+              <td>
+                RM{" "}
+                {(parseInt(data.pricePerDay) * parseInt(data.diffTime)).toFixed(
+                  2
+                )}
+              </td>
             </tr>
-            <tr className="">
+            {parseInt(data.pricePerDay) * parseInt(data.diffTime) !==
+              parseInt(data.totalPrice) && (
+              <tr className="border-b-2 border-black">
+                <table className="w-full mt-4">
+                  <tbody>
+                    <tr>
+                      <td className="w-1/3 pb-6 text-left font-semibold">
+                        Discount
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <td></td>
+                <td>
+                  - RM{" "}
+                  {(
+                    parseInt(data.pricePerDay) * parseInt(data.diffTime) -
+                    parseInt(data.totalPrice)
+                  ).toFixed(2)}
+                </td>
+              </tr>
+            )}
+            <tr>
               <td></td>
               <td className="text-xl font-bold p-6">Grand Total</td>
-              <td className=" text-xl font-bold">RM 300</td>
+              <td className=" text-xl font-bold">
+                RM {parseInt(data.totalPrice).toFixed(2)}
+              </td>
             </tr>
             <tr>
               <td></td>
               <td></td>
               <td>
                 <button
-                  type="button"
-                  className="mt-10 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-zinc-900 hover:bg-stone-600 text-white font-bold py-2 px-4 mr-1 rounded tracking-wider"
+                  onClick={() => window.print()}
+                  className="mt-10 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-zinc-900 hover:bg-stone-600 text-white font-bold py-2 px-4 mr-1 rounded tracking-wider print:hidden"
                 >
-                  Share
+                  Print
                 </button>
               </td>
             </tr>
